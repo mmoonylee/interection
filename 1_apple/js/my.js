414f10d5a65b0a2844ed40dc2f60bbeb37c46a1c
moony <mmoonylee@gmail.com>
2023-03-14 22:12:58 +0900

    오늘까지.

--- 1_apple/js/my.js
+++ 1_apple/js/my.js
@@ -1,5 +1,7 @@
 (() => {
     let yOffset = 0; //window.pageYOffset 대신 쓸 변수
+    let prevScrollHeight = 0;
+    let currentScene = 0;
 
     const sceneInfo = [
         // 각 객체는 섹션을 나타낸다
@@ -9,7 +11,14 @@
             heightNum: 5, //브라우저 높이의 5배로 scrollHeight 세팅
             scrollHeight : 0,
             objs:{
-                container: document.querySelector("#scroll-section-0")
+                container: document.querySelector('#scroll-section-0'),
+                messageA: document.querySelector('#scroll-section-0 .main-message.a'),
+				messageB: document.querySelector('#scroll-section-0 .main-message.b'),
+				messageC: document.querySelector('#scroll-section-0 .main-message.c'),
+				messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+            },
+            values:{
+
             }
         },
         {
@@ -47,14 +56,47 @@
             sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
         }
-        console.log(sceneInfo)
+
+        yOffset = window.pageYOffset;
+        let totalScrollHeight = 0;
+		for (let i = 0; i < sceneInfo.length; i++) {
+			totalScrollHeight += sceneInfo[i].scrollHeight;
+			if (totalScrollHeight >= yOffset) {
+				currentScene = i;
+				break;
+			}
+		}
     }
 
     function scrollLoop(){
+        prevScrollHeight = 0; 
+        for (let i = 0; i < currentScene; i++) {
+			prevScrollHeight += sceneInfo[i].scrollHeight;
+		}
         
+        //스크롤 내릴때 
+        if ( yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight){
+            currentScene++;
+        }
+
+        // 스크롤 올릴때
+        if ( yOffset < prevScrollHeight){
+            if( currentScene === 0) return; //바운스 동작의 오류 방지
+            currentScene--;
+        }
+
+        document.body.setAttribute('id', `show-scene-${currentScene}`);
+        console.log(currentScene)
     }
+
+    function playAnimation(){
+        
+
+    }
+    
     setLayout();
 
+    window.addEventListener("load", setLayout);
     window.addEventListener("resize", setLayout);
     window.addEventListener("scroll", () => {
         yOffset = window.pageYOffset;
